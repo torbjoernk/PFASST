c6414257b13d3ea2302703015a6933fc806eae8e
Torbjörn Klatt <t.klatt@fz-juelich.de>
2015-10-14 10:47:23 +0200

    util: split string into vector
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

--- include/pfasst/util.hpp
+++ include/pfasst/util.hpp
@@ -149,6 +149,24 @@ namespace pfasst
     return "";
 #endif
   }
+
+
+  static vector<string>& split(const string& s, char delim, vector<string>& elems)
+  {
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim)) {
+      elems.push_back(item);
+    }
+    return elems;
+  }
+
+  static vector<string> split(const string& s, char delim)
+  {
+    vector<string> elems;
+    split(s, delim, elems);
+    return elems;
+  }
 }  // ::pfasst
 
 #endif  // _PFASST__UTIL_HPP_
