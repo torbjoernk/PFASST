6148fd8726a7d8c79adbd9cfe3e2597505fd1678
Torbjörn Klatt <t.klatt@fz-juelich.de>
2016-03-29 15:42:59 +0200

    [cmake] use CMake's new-ish cxx-features
    Let CMake discover the required C++ standard and set appropriate
    compiler flags.
    
    Signed-off-by: Torbjörn Klatt <t.klatt@fz-juelich.de>

--- 3rdparty/Dune.cmake
+++ 3rdparty/Dune.cmake
@@ -29,6 +29,15 @@ if(dune-common_DIR AND dune-istl_DIR AND dune-localfunctions_DIR AND dune-grid_D
 
     message(STATUS "  include path: ${dune_INCLUDE_PATHS}")
     message(STATUS "  libraries: ${dune_LIBRARIES}")
+
+    set(CMAKE_CXX_STANDARD 14)
+    set(dune_CXX_FEATURES
+        ${pfasst_CXX_FEATURES}
+    )
+    message(STATUS "!! Dune requires C++14 for some parts.")
+    message(STATUS "    please use both:")
+    message(STATUS "      set_property(TARGET tgt PROPERTY CXX_STANDARD 11)")
+    message(STATUS "      target_compile_features(tgt PUBLIC \${dune_CXX_FEATURES})")
 else()
     message(STATUS "!! not found")
     set(dune_FOUND FALSE)
@@ -37,3 +46,5 @@ endif()
 set(dune_FOUND ${dune_FOUND} PARENT_SCOPE)
 set(dune_INCLUDE_PATHS ${dune_INCLUDE_PATHS} PARENT_SCOPE)
 set(dune_LIBRARIES ${dune_LIBRARIES} PARENT_SCOPE)
+set(CMAKE_CXX_STANDARD ${CMAKE_CXX_STANDARD} PARENT_SCOPE)
+set(dune_CXX_FEATURES ${dune_CXX_FEATURES} PARENT_SCOPE)
